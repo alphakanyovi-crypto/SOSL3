fbf24963b69029abe8256b52017bf3494c63047e
ALPHA <alphakanyovi@gmail.com>
2026-05-29 16:31:35 +0300

    updated navbar

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <script setup>
 import Home from './views/Home.vue';
 import{ RouterView } from 'vue-router';
-import Navbar from './components/Navbar.vue';
+import Navbar from './components/navbar.vue';
 import Footer from './components/Footer.vue';
 </script>
 <template>
